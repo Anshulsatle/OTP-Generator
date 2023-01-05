689072c63fe0b752a4e9dfa1478a16e21dad1fd8
Anshulsatle <54183215+Anshulsatle@users.noreply.github.com>
2023-01-05 16:06:54 +0530

    Add files via upload

---
+++ Otp.cpp
@@ -0,0 +1,33 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+string generateOTP(int len)
+{
+	
+	string str = "abcdefghijklmnopqrstuvwxyzABCD"
+			"EFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int n = str.length();
+
+	
+	string OTP;
+
+	for (int i=1; i<=len; i++)
+		OTP.push_back(str[rand() % n]);
+
+	return(OTP);
+}
+
+int main()
+{
+	
+	srand(time(NULL));
+
+	
+	int len = 6;
+	printf("Your OTP is - %s", generateOTP(len).c_str());
+
+	return(0);
+}
+
